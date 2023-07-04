662e3898f39228e9bc093cc81a6444db67eed49e
Sangeet Sharma <sangeetsharmaofficial@gmail.com>
2023-07-05 05:08:26 +0530

    created the login and register page and created the functionality to fetch the data input by the user

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import {Routes, Route} from 'react-router-dom'
 import HomePage from './pages/homePage';
+import Register from './pages/Register';
+import Login from './pages/login';
 function App() {
   return (
     <>
     <Routes>
       <Route path='/' element={<HomePage />}></Route>
+      <Route path="/register" element={<Register/>}/>
+      <Route path='/login' element={<Login/>}/>
     </Routes>
     </>
   );
